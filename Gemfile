37e961414a1aeab23f69f54d081e7b14a5ccefc2
Laura Bello <Laura@Cristians-MacBook-Pro-2.local>
2018-05-08 20:01:00 -0500

    Agregando gráficos

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+  gem 'jquery-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -53,6 +54,7 @@ group :development, :test do
   gem "database_cleaner"
 
   gem "letter_opener"
+
 
 end
 
--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,8 +1,30 @@
 class ExpensesController < ApplicationController
-  def index
-    @expenses = Expense.all
-    respond_to do |format|
-    format.html { render :index}
-    format.html { render json: @expenses, status: ok}
-  end
+
+ def index
+
+     transaction_type = params["transaction_type"]
+     category = params[:category]
+
+     if transaction_type && !category
+         @expenses = Expense.joins(:transaction_type).where("transaction_types.name = ?", transaction_type.capitalize)
+     elsif category && !transaction_type
+         @expenses = Expense.joins(:category).where("categories.name = ?", category.capitalize)
+     elsif category && transaction_type
+         @expenses = Expense.joins(:transaction_type, :category).where("transaction_types.name = ? AND categories.name = ?",
+         transaction_type.capitalize, category.capitalize)
+     else
+         @expenses = Expense.all
+     end
+
+     @transaction_types = TransactionType.all
+     @category = Category.all
+
+     respond_to do |format|
+         format.html { render :index}
+         format.json { render json: @expenses, status: :ok}
+         format.js {render partial: "expenses_list", locals: { expenses: @expenses }}
+     end
+
+ end
+
 end
